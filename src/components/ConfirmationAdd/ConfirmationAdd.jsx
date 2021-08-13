0e2c774ee9d851380f46a273e2858256553ad1e2
Meghan Cleland <mplscleland@gmail.com>
2021-08-12 22:19:10 -0500

    fixed Confirmation page

--- src/components/ConfirmationAdd/ConfirmationAdd.jsx
+++ src/components/ConfirmationAdd/ConfirmationAdd.jsx
@@ -1,18 +1,22 @@
+import { Button } from "@material-ui/core";
 import { useHistory } from "react-router";
+import './ConfirmationAdd.css';
+
+
 function ConfirmationAdd() {
   const history = useHistory();
   const handleBrowse = () => {
     history.push(`/barber`);
   };
   return (
-    <>
+    <div id="confirmation">
       <h1> Thanks for contributing to Queer Shears!</h1>
       <p>Submissions are reviewed by our admins to ensure correct information.</p>
       <p>Posts with inaccurate information, or duplicate info will be removed.</p>
-      <button type="button" onClick={handleBrowse}>
+      <Button type="button" onClick={handleBrowse}>
         Browse Barbers
-      </button>
-    </>
+      </Button>
+    </div>
   );
 }
 
